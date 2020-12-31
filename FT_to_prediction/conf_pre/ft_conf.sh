32756e12e8a67b9467612334ba7467d1a67f372f
xuedy <897741007@qq.com>
2020-12-31 09:31:28 +0800

    Update ft_conf.sh

--- FT_to_prediction/conf_pre/ft_conf.sh
+++ FT_to_prediction/conf_pre/ft_conf.sh
@@ -15,20 +15,23 @@ testdata_dir=""
 
 ### attention, this term need to be modified
 vocab_path="./package/mol/molecule_dict"
+
 ### attention, this term need to be modified
 CONFIG_PATH="./package/mol/ernie_config.json"
 
 
 stream_job=""
 finetuning_task=$task
-#
+
+### attention, this term need to be modified
 finetuning_data="path_to_training_data_folder"
 
 nodes=1
 
-#init_model=""
 ### attention, this term need to be modified
 init_model="./data/model/step_400000"
+#init_model=""
+
 #CKPT_PATH="./checkpoints/step_54000"
 other_data=""
 
@@ -41,23 +44,32 @@ loss_scaling=128
 generate_neg_sample="False"
 
 IS_DISTRIBUTED="false"
+
 ### attention, this term need to be modified
 EPOCH=40
+
 ### attention, this term need to be modified
 VALID_STEPS=1800
+
 SAVE_STEPS=5400
 WARMUP_STEPS=0
+
 ### attention, this term need to be modified
 BATCH_SIZE=16
+
 NUM_HEAD=12
+
 ### attention, this term need to be modified
 LR_RATE=5e-5
+
 WEIGHT_DECAY=0.01
 D_MODEL=768
 NUM_LAYER=12
 MAX_LEN=512
 export TOKENIZER="MolTokenizer"
+
 ### attention, this term need to be modified
+### in regression tasks, the value of num_labels would not influence the task
 num_labels=2
 
 #export
